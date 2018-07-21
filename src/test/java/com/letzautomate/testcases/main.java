d7073aa1cd192a929a063df70b357acebfdebefe
Ramakrishna Jangatisetty <letzautomate@gmail.com>
2018-07-21 19:42:57 +0530

    Initial Implementation, 3 drivers Parallel Exection, Open Browsers

---
+++ src/test/java/com/letzautomate/testcases/main.java
@@ -0,0 +1,21 @@
+package com.letzautomate.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import com.letzautomate.Selenium.WebDriverManager;
+
+public class main {
+	WebDriver driver;
+	
+	@Test
+	@Parameters({"browser"})
+	public void runSuite(String browser){	
+		driver = new WebDriverManager().getLocalInstance(browser);
+		driver.get("http://newtours.demoaut.com");
+		driver.manage().window().maximize();
+	}
+	
+
+}
